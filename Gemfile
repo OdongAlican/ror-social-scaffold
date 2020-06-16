1b92e081fc7269aa0019d0cad6fdfaa3486700c0
Phylis Chepchumba <chumba.phyl@gmail.com>
2020-06-16 16:30:57 +0300

    Fix linter errors

--- Gemfile
+++ Gemfile
@@ -12,8 +12,8 @@ gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
-gem 'uglifier', '>= 1.3.0'
 gem 'rubocop'
+gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
 
@@ -47,7 +47,7 @@ group :development, :test do
 end
 
 group :test do
-# Adds support for Capybara system testing and selenium driver
+  # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
   # Easy installation and use of web drivers to run system tests with browsers
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,25 +15,24 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :friend_requests
-  has_many :inverse_friend_requests, :class_name => "FriendRequest", :foreign_key => "friend_id"
-
+  has_many :inverse_friend_requests, class_name: 'FriendRequest', foreign_key: 'friend_id'
 
   def friends
-    friends_array = friend_requests.map{|friendship| friendship.friend if friendship.confirmed}
-    friends_array + inverse_friend_requests.map{|friendship| friendship.user if friendship.confirmed}
+    friends_array = friend_requests.map { |friendship| friendship.friend if friendship.confirmed }
+    friends_array + inverse_friend_requests.map { |friendship| friendship.user if friendship.confirmed }
     friends_array.compact
   end
 
   def pending_friends
-    friend_requests.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+    friend_requests.map { |friendship| friendship.friend unless friendship.confirmed }.compact
   end
 
   def friend_requests
-    inverse_friend_requests.map{|friendship| friendship.user if !friendship.confirmed}.compact
+    inverse_friend_requests.map { |friendship| friendship.user unless friendship.confirmed }.compact
   end
 
   def confirm_friend(user)
-    friendship = inverse_friend_requests.find{|friendship| friendship.user == user}
+    friendship = inverse_friend_requests.find { |friendship| friendship.user == user }
     friendship.confirmed = true
     friendship.save
   end
